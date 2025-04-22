a87141b757fb9af6e2e6d144aeaaf3f3c76f2c1b
Erik Fudge <fudgee@merrimack.edu>
2025-04-22 16:22:34 -0400

    scoreboard upload

--- Scoreboard.java
+++ Scoreboard.java
@@ -2,18 +2,24 @@ import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Scoreboard {
-    int scoreboard;
-    Scoreboard(){
+    // Class variable if needed
+    public PriorityQueue<Integer> scores;
+    int leaderboard;
+    int wins;
 
+
+    // Constructor
+    public Scoreboard() {
     }
-    public static void main(String[] args) {
-        PriorityQueue<Integer> scoreboard = new PriorityQueue<>();
-            Scanner players = new Scanner(System.in);
-            System.out.print("Player 1 enter name");
-            String playerone = players.toString();
-            System.out.print("Player 2 enter name");
-            String playertwo = players.toString();
-            System.out.println(players);
 
+    public static void main(String[] args) {
+        Scanner players = new Scanner(System.in);
+        System.out.print("Player 1 enter name: ");
+        String playerone = players.next();
+        System.out.print("Player 2 enter name: ");
+        String playertwo = players.next();
+        System.out.println(playertwo + playerone);
+        // Close the Scanner when done
+        players.close();
     }
 }
